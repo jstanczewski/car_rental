9d69c18ad9bd3c130c0f0486aec31539f86b5545
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2021-05-26 19:23:33 +0200

    validation rules for choosing same insurance

--- viewer/forms.py
+++ viewer/forms.py
@@ -15,4 +15,10 @@ class ContractForm(ModelForm):
             raise ValidationError("Date to cannot come before date from!")
         elif result["date_from"] < date.today():
             raise ValidationError("Past dates are not allowed!")
+        if result["insurance_1_id"] is not None and (result["insurance_1_id"] == result["insurance_2_id"] or result["insurance_1_id"] == result["insurance_3_id"] or result["insurance_1_id"] == result["insurance_4_id"]):
+            raise ValidationError("Cannot choose same insurance more than once!")
+        if result["insurance_2_id"] is not None and (result["insurance_2_id"] == result["insurance_3_id"] or result["insurance_2_id"] == result["insurance_4_id"]):
+            raise ValidationError("Cannot choose same insurance more than once!")
+        if result["insurance_3_id"] is not None and (result["insurance_3_id"] == result["insurance_4_id"]):
+            raise ValidationError("Cannot choose same insurance more than once!")
         return result
